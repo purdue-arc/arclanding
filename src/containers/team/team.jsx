2b24b9ab5cbda12f5ea98b9a2c5bf8bfcaea9944
pmondaPUWL <pmonda@purdue.edu>
2024-03-04 18:52:39 -0500

    finished editing team pages

--- src/containers/team/team.jsx
+++ src/containers/team/team.jsx
@@ -5,22 +5,22 @@ import arcicon from "../../assets/arcicon.png"
 const Team = () => {
     const teamMembers = [
         { role: "President", name: "Haddy Alchaer", division: "Executive Board" },
-        { roles: ["President","Project Lead" ], name: "Revanth Senthilkumaran", division: "Executive Board"},
+        {roles: ["President","Project Lead" ], name: "Revanth Senthilkumaran", division: "Executive Board"},
         { role: "Operations Director", name: "Mouli Sangita", division: "Executive Board" },
         { role: "Treasurer", name: "Tamara Itani", division: "Executive Board" },
-        // { role: "Project Lead", name: "Revanth Senthilkumaran", division: "Project Leads" },,
-        { role: "Project Lead", name: "Josh Mansky", division: "Project Leads" },
-        { role: "Project Lead", name: "Ryan Ting", division: "Project Leads" },
-        { role: "Project Lead", name: "Campbell McClendon", division: "Project Leads" },
-        { role: "Project Lead", name: "Sam Graham", division: "Project Leads" },
-        { role: "Project Lead", name: "Aarav Garg", division: "Project Leads" },
-        { role: "Operations Lead", name: "Nithin Anand", division: "Operations Leads" },
-        { role: "Operations Lead", name: "Edgar Babajanyan", division: "Operations Leads" },
-        { role: "Operations Lead", name: "Yashvi Agrawal", division: "Operations Leads" },
-        { role: "Operations Lead", name: "Pranav Krishnamoorthy", division: "Operations Leads" },
+        { role: "Piano Hand Lead", name: "Revanth Senthilkumaran", division: "Project Leads" },
+        { role: "Drone Delivery Lead", name: "Josh Mansky", division: "Project Leads" },
+        { role: "Dog Copter Lead", name: "Ryan Ting", division: "Project Leads" },
+        { role: "Rocket League Lead", name: "Campbell McClendon", division: "Project Leads" },
+        { role: "Wizard's Chess Lead", name: "Sam Graham", division: "Project Leads" },
+        { role: "Sphero Swarm Lead", name: "Aarav Garg", division: "Project Leads" },
+        { role: "Events Coordinator", name: "Nithin Anand", division: "Operations Leads" },
+        { role: "Development Lead", name: "Edgar Babajanyan", division: "Operations Leads" },
+        { role: "Marketing Lead", name: "Yashvi Agrawal", division: "Operations Leads" },
+        { role: "Outreach / Sponsorships Lead", name: "Tristan Brideweser", division: "Operations Leads" },
         { roles: ["Product & Design Lead", "RISE Lead"], name: "Vijay Muthukumar", division: "RISE Lead" },
     ];
-
+   
     return (
         <div className="team__container">
             {teamMembers.map((member, index) => (
